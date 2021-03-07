357fd3fa17214237246c93e361989c133b1c8cd1
Patrick Y. Wu <pywu@umich.edu>
2021-03-07 02:35:18 -0500

    updated partisan association scores

--- partisanassociations/partisanassociations.py
+++ partisanassociations/partisanassociations.py
@@ -1,5 +1,7 @@
 from gensim.models.doc2vec import Doc2Vec, TaggedDocument
 from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
+import pandas as pd
 
 def word_embeddings_spaces(descriptions, total_passes=10, n_dim=150, epochs=100, window=10, alpha=0.03, min_alpha=0.03, workers=2, min_count=8, dm=0, dbow_words=1, save_by_iteration=True):
     '''
@@ -24,3 +26,54 @@ def word_embeddings_spaces(descriptions, total_passes=10, n_dim=150, epochs=100,
             file_name = f'iter{i}_d2v.pkl'
             desc_d2v.save(file_name)
     return iterations
+
+def produce_subspace_vector(positive, negative, model):
+    psum = np.zeros(model[0].shape[0])
+    nsum = np.zeros(model[0].shape[0])
+
+    for p in positive:
+        psum += model.wv[p]
+    if len(positive) > 1:
+        psum /= len(positive)
+
+    for n in negative:
+        nsum += model.wv[n]
+    if len(negative) > 1:
+        nsum /= len(negative)
+
+    return psum - nsum
+
+def partisan_associations(iterations, pairs_words, ids):
+    """
+    iterations: a dictionary of iterations
+    pairs_words: a list of lists; each individual list is of length 2 and consists of a list of positive words and a list of negative words. Either of the lists can be of length 0.
+    ids: a list of document ids, used to construct the TaggedDocument
+    """
+    PA_scores = []
+
+    for i in range(len(iterations)):
+        iteration_name = f'iter{i}'
+        mod = iterations[iteration_name]
+        docvecs = [mod.docvecs[x] for x in ids]
+        df_iteration = pd.DataFrame(ids, columns=['id'])
+
+        for pair in pairs_words:
+            subspace = produce_subspace_vector(positive=pair[0], negative=pair[1], model=mod)
+            subspace = np.expand_dims(subspace, axis=0)
+            cs = cosine_similarity(docvecs, subspace)
+            variable_name_pos = ''.join(pair[0])
+            variable_name_neg = ''.join(pair[1])
+            variable_name = '_'.join([variable_name_pos,variable_name_neg])
+            df_iteration[variable_name] = cs.squeeze(axis=1)
+
+        PA_scores.append(df_iteration)
+
+    return PA_scores
+
+def averaging_pa_scores(PA_scores):
+    PA_scores_concat = pd.concat(PA_scores)
+    byid = PA_scores_concat.groupby('id')
+    PA_scores_mean = byid.mean()
+    PA_scores_mean = PA_scores_mean.reset_index()
+
+    return PA_scores_mean
